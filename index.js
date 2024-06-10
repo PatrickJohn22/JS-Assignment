eb1a015c59b2fdf898c365b750f2cf7105fc37c3
PatrickJohn22 <jpgokum@gmail.com>
2024-06-10 16:05:15 +0100

    My first commit

---
+++ index.js
@@ -0,0 +1,160 @@
+
+/**
+ * Create a program
+ * THAT TAKES ONE INPUT FROM THE USER (number),
+ * IF IT IS NOT A NUMBER (TELL THE USER TO INPUT ONLY NUMERS)
+ * PERFORM AN OPERATION (%)
+ * OUTPUT THE RESULT AND MODULUS TO THE USER AND COSNOLE
+Expand
+index.js
+2 KB
+﻿
+/**
+ * Create a program
+ * THAT TAKES ONE INPUT FROM THE USER (number),
+ * IF IT IS NOT A NUMBER (TELL THE USER TO INPUT ONLY NUMERS)
+ * PERFORM AN OPERATION (%)
+ * OUTPUT THE RESULT AND MODULUS TO THE USER AND COSNOLE
+ */
+
+
+// GLOBAL VARAIBLE
+const a = 10;
+const b = 20;
+const linebreak = "<br />";
+document.write('A =');
+document.write(a)
+document.write(linebreak)
+document.write('B =');
+document.write(b);
+document.write(linebreak)
+document.write("what is (a==b ) ? => ");
+
+result = (a == b);
+document.write(result);
+document.write()
+
+// greater than
+document.write(linebreak)
+document.write("what is (a > b ) ? => ");
+
+result = (a > b);
+document.write(result);
+document.write()
+
+
+// greater than or equal to
+document.write(linebreak)
+document.write("what is (a >= b ) ? => ");
+
+result = (a >= b);
+document.write(result);
+document.write()
+
+
+// decide 5 different operations that results in true 
+document.write(linebreak)
+const m = 50;
+const n = 20;
+
+const q = (m > n) ? true : false
+document.write(linebreak)
+document.write('Result: ', q);
+
+const naija = 200;
+const ghana = 100;
+const w = (naija> ghana) ? true : false;
+document.write(linebreak)
+document.write('Result: ', w);
+
+const country = 500;
+const state = 300;
+const k = (country> state) ? true : false;
+document.write(linebreak)
+document.write('Result: ', w);
+
+const women = 500;
+const men = 300;
+const j = (women > men) ? true : false;
+document.write(linebreak)
+document.write('Result: ', j);
+
+const lagos = 100;
+const kaduna = 50;
+const t = (lagos > kaduna) ? true : false;
+document.write(linebreak)
+document.write('Result: ', t);
+
+// (logical operations) 5 each (true and false)
+document.write(linebreak)
+document.write(linebreak)
+document.write("what is (k > j ) ? => ");
+
+result = (k > j);
+document.write(result);
+
+
+const e = 50
+const d = 40
+const l = (d < e) ? true : false;
+document.write(linebreak)
+document.write('Result: ', l);
+
+
+const f = 1000
+const p = 200
+const h = (f <= p ) ? true : false;
+document.write(linebreak)
+document.write('Result: ', h);
+
+
+const s = 1000
+const v = 200
+const o = (s >= v ) ? true : false;
+document.write(linebreak)
+document.write('Result: ', k);
+
+const labour = 1000;
+const fg = 200;
+const nija = (labour > fg ) ? true : false;
+document.write(linebreak)
+document.write('Result: ', nija);
+document.write(linebreak)
+// Assignments (modules, multipication, etc)
+document.write(linebreak)
+const x = 30;
+const y = 70;
+const r = (x + y)
+document.write('Result:',r )
+
+document.write(linebreak)
+const week = 30;
+const keep = 70;
+const wet = (week * keep)
+document.write('Result:',wet )
+
+document.write(linebreak)
+const congo = 30;
+const cameroon = 70;
+const africa = (congo / cameroon)
+document.write('Result:',africa )
+
+document.write(linebreak)
+const garbon = 30;
+const angola = 70;
+const kenya = (angola - garbon )
+document.write('Result:',kenya )
+
+document.write(linebreak)
+const japan = 30;
+const china = 70;
+const isreal = (japan % china )
+document.write('Result:',isreal )
+
+
+// conditional operator
+
+
+
+
+// create conditional operation (10 of them)
